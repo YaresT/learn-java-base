ad56c2a712bcb13078de4797ed1a648bf86df5d1
Ярослав Тарасв <y.tarasov@innopolis.ru>
2020-08-30 01:00:15 +0400

    [Lesson 30] Сделал дополнительное ДЗ 30 - Динамическое нахождение медианного элемента

---
+++ src/main/java/lesson30/MedianIntegerStream.java
@@ -0,0 +1,40 @@
+package lesson30;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class MedianIntegerStream {
+    private final Queue<Integer> minHeap;
+
+    private final Queue<Integer> maxHeap;
+
+    public MedianIntegerStream() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+    }
+
+    public void add(final int num) {
+        if (minHeap.size() == maxHeap.size()) {
+            maxHeap.offer(num);
+            minHeap.offer(maxHeap.poll());
+        } else {
+            minHeap.offer(num);
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public double getMedian() {
+        int median = 0;
+
+        if (!maxHeap.isEmpty() && !minHeap.isEmpty()) {
+            if (minHeap.size() > maxHeap.size()) {
+                median = minHeap.peek();
+            } else {
+                median = (minHeap.peek() + maxHeap.peek()) / 2;
+            }
+        }
+
+        return median;
+    }
+}
